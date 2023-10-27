a3ddbe06b338246a6414d22d2311a3121a338a64
thi4go-git <thi4go19@gmail.com>
2023-10-27 15:06:39 -0300

    Exempolos 27/10

--- javascript/index.js
+++ javascript/index.js
@@ -13,11 +13,33 @@ comentário
 
 // Ler variáveis no Beecrowd semelhante ao prompt: lines.shift()
 // exemplo:
+
+//para ler 1 valor de 1 linha ex: 3.0
 // let valor = parseInt(lines.shift());
 
-let nrFuncionario = 6;
-let hrTrabalhada = 145;
-let vlrHoraTrabalhada = 15.55;
+//para ler mas de 1 valor de 1 linha ex: 3.0 4.0 5.2
+// let linha = lines.shift().split(" ")
+//  linha[0] -> 3.0 linha[1] -> 4.0  linha[2] -> 5.2
 
-console.log("NUMBER = "+nrFuncionario);
-console.log("SALARY = U$ "+(hrTrabalhada*vlrHoraTrabalhada).toFixed(2));
+
+let A = 3.0;
+let B = 4.0;
+let C = 5.2;
+
+const pi = 3.14159;
+let areaTriangulo = ((A * C) / 2).toFixed(3);
+let areaCirculo = (pi * C * C).toFixed(3);
+let areaTrapezio = (((A + B) * C) / 2).toFixed(3);
+let areaQuadrado = (B * B).toFixed(3);
+let areaRetangulo = (A * B).toFixed(3);
+
+console.log(areaTriangulo);
+console.log(areaCirculo);
+console.log(areaTrapezio);
+console.log(areaQuadrado);
+console.log(areaRetangulo);
+
+
+
+
+
