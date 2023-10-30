9332f8bdae5233d2a8759d1ffc48cb9d3375bed6
thi4go-git <thi4go19@gmail.com>
2023-10-30 14:30:04 -0300

    Matriz e Vetor

--- javascript/index.js
+++ javascript/index.js
@@ -22,24 +22,3 @@ comentário
 //  linha[0] -> 3.0 linha[1] -> 4.0  linha[2] -> 5.2
 
 
-let A = 3.0;
-let B = 4.0;
-let C = 5.2;
-
-const pi = 3.14159;
-let areaTriangulo = ((A * C) / 2).toFixed(3);
-let areaCirculo = (pi * C * C).toFixed(3);
-let areaTrapezio = (((A + B) * C) / 2).toFixed(3);
-let areaQuadrado = (B * B).toFixed(3);
-let areaRetangulo = (A * B).toFixed(3);
-
-console.log(areaTriangulo);
-console.log(areaCirculo);
-console.log(areaTrapezio);
-console.log(areaQuadrado);
-console.log(areaRetangulo);
-
-
-
-
-
